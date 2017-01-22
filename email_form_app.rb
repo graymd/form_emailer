98833318c0415a032ee379d924c9e0c441666eac
Malachi Gray <graymd.cfi@gmail.com>
2017-01-21 22:40:04 -0500

    remove commented out text

--- email_form_app.rb
+++ email_form_app.rb
@@ -5,11 +5,9 @@ require 'pry'
 require 'json'
 require 'sinatra/cross_origin'
 
-# if Sinatra::Base.development?
 configure do
   enable :cross_origin
 end
-# end
 
 if Sinatra::Base.production?
   set :allow_origin, ENV["ALLOWED_ORIGIN"]
